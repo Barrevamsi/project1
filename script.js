c5d80940f7ea164626618f2e1e99a4cbd0a1e3aa
Barrevamsi <137883602+Barrevamsi@users.noreply.github.com>
2023-07-19 11:08:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function egghatching()
+{
+    document.getElementById("egg").src="https://media.istockphoto.com/id/1341079712/vector/the-hen-hatches-the-eggs-in-the-nest-cartoon-vector-illustration.jpg?s=612x612&w=0&k=20&c=UriKUUwvBSJ4tRxPPqFc8wIyi21ddwEj5WHAsZJVm68=" ;
+    
+}
+function egghatching1()
+{
+    document.getElementById("egg").src="https://www.shutterstock.com/image-vector/cartoon-cute-chic-hatching-easter-600w-591984062.jpg" ;
+}
+function egghatching2()
+{
+    document.getElementById("egg").src="https://www.shutterstock.com/image-vector/funny-yellow-newborn-chicken-broken-600w-746881516.jpg" ;
+}
